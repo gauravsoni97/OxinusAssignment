d67ea946cfcd03a156359985e68ed20cb580821d
g.soni <g.soni@caddlance.com>
2024-06-21 21:35:07 +0530

    Feat: About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor at blanditiis voluptates quaerat, saepe nemo et tempora labore nulla! Quidem repellat facilis et incidunt labore corporis delectus, expedita quasi illum!</div>
-  )
-}
+    <div className="max-w-screen-xl mx-auto mt-16">
+      <p class="mb-3 text-gray-500 dark:text-gray-400">
+        Welcome to my website! I'm passionate about creating delightful web
+        experiences that blend functionality with aesthetic appeal. As a
+        developer, I thrive on crafting intuitive user interfaces and seamless
+        interactions using modern web technologies.
+      </p>
+      <p class="mb-3 text-gray-500 dark:text-gray-400">
+        My journey in web development began with a curiosity to build things
+        that not only work well but also bring joy to those who use them. I
+        believe in the power of clean code and thoughtful design to enhance user
+        experience.
+      </p>
+      <p class="mb-3 text-gray-500 dark:text-gray-400">
+        Outside of coding, I enjoy exploring new cuisines and experimenting with
+        recipes. This project combines my love for technology with my
+        appreciation for food, using the MealDB API to showcase a store's menu
+        and provide users with features like saving their favorite meals and
+        generating random meal suggestions.
+      </p>
 
-export default About
+      <p class="mb-3 text-gray-500 dark:text-gray-400">
+        Feel free to explore the site, discover new meals, and don't hesitate to
+        reach out if you have any questions or feedback. Thank you for visiting!
+      </p>
+      <hr class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"/>
+      <p class="mb-3 text-gray-500 dark:text-gray-400">
+        Project Designed and Developed by: Gaurav Soni | gauravsoni8414@gmail.com | +91 8053340056
+      </p>
+    </div>
+  );
+};
+
+export default About;
