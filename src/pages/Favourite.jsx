42ad15914125b49f4f7b800bc437d250aacf7102
g.soni <g.soni@caddlance.com>
2024-06-21 21:28:03 +0530

    Feat : layout set for favourite

--- src/pages/Favourite.jsx
+++ src/pages/Favourite.jsx
@@ -3,11 +3,10 @@
 import React from "react";
 
 const Favorite = () => {
-
   const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
   return (
-    <div className="max-w-screen-xl mx-auto mt-12 p-6">
+    <div className="max-w-screen-lg mx-auto mt-12 p-6">
       <h1 className="text-3xl font-extrabold leading-none tracking-tight text-gray-300">
         Favorite Meals
       </h1>
@@ -15,15 +14,16 @@ const Favorite = () => {
         {favorites.map((meal) => (
           <li
             key={meal.idMeal}
-            className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 relative"
+            class="max-w-xs mx-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 relative"
           >
             <img
-              className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+              class="rounded-t-lg"
               src={meal.strMealThumb}
               alt={meal.strMeal}
             />
-            <div className="flex flex-col justify-between p-4 leading-normal">
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+
+            <div class="p-5">
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
                 {meal.strMeal}
               </p>
             </div>
